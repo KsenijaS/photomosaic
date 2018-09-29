488e396ec75b97e07b7ee4cd7d7806e8924b2bd6
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2018-09-29 13:11:50 -0700

    Create Image Matrix

---
+++ image_matrix.go
@@ -0,0 +1,58 @@
+package photomosaic
+
+import (
+	"errors"
+	"image"
+	"fmt"
+)
+
+const (
+	subimageWidthPx = 200
+	subimageHeightPx = 150
+)
+
+type ImageMatrix struct {
+	img *image.NRGBA
+}
+
+func NewImageMatrix(i *image.NRGBA) (*ImageMatrix, error) {
+	b := i.Bounds()
+	w := b.Max.X - b.Min.X
+	h := b.Max.Y - b.Min.Y
+
+	if (w % subimageWidthPx != 0) {
+		err := errors.New(fmt.Sprintf("width must be divisible by %d", subimageWidthPx))
+		return nil, err
+	}
+
+	if (h % subimageHeightPx != 0) {
+		err := errors.New(fmt.Sprintf("height must be divisible by %d", subimageHeightPx))
+		return nil, err
+	}
+
+	return &ImageMatrix{img: i}, nil
+}
+
+func (p *ImageMatrix) genSubImageRectangles() []image.Rectangle {
+        var out []image.Rectangle
+        b := p.img.Bounds()
+
+        for y := b.Min.Y; y < b.Max.Y; y += subimageHeightPx {
+                for x:= b.Min.X; x < b.Max.X; x += subimageWidthPx {
+                        out = append(out, image.Rect(x, y, x+subimageWidthPx, y+subimageHeightPx))
+                }
+        }
+        return out
+}
+
+
+func (p *ImageMatrix) SubImages() []image.Image {
+	listRec := p.genSubImageRectangles()
+	out := make([]image.Image, len(listRec))
+
+	for i := range listRec {
+		out[i] = p.img.SubImage(listRec[i])
+	}
+	return out
+}
+
---
+++ image_matrix_test.go
@@ -0,0 +1,108 @@
+package photomosaic
+
+import (
+	"image"
+	"testing"
+	"strings"
+	"image/draw"
+	"image/color"
+)
+
+func TestNewImageMatrixAcceptsImageOfSubimageSize(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(100, 100, 300, 250))
+	im, err := NewImageMatrix(input)
+	if err != nil {
+		t.Errorf("Error while creating the image: %s", err)
+	}
+	if im == nil {
+		t.Errorf("Image was nil")
+	}
+	if input != im.img {
+		t.Errorf("Input image was not stored in the returned struct")
+	}
+}
+
+func TestNewImageMatrixRejectsIncompatibleWidthImage(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(100, 100, 123, 250))
+	im, err := NewImageMatrix(input)
+	if err == nil {
+		t.Errorf("NewImageMatrix accepted an image of incompatible width")
+	}
+	if !strings.Contains(err.Error(), "width") {
+		t.Errorf("Returned error message did not mention the incompatible width: %s", err)
+	}
+	if im != nil {
+		t.Errorf("NewImageMatrix returned both an image and an error for an image of incompatible width")
+	}
+}
+
+func TestNewImageMatrixRejectsIncompatibleHeightImage(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(100, 100, 300, 123))
+	im, err := NewImageMatrix(input)
+	if err == nil {
+		t.Errorf("NewImageMatrix accepted an image of incompatible height")
+	}
+	if !strings.Contains(err.Error(), "height") {
+		t.Errorf("Returned error message did not mention the incompatible height: %s", err)
+	}
+	if im != nil {
+		t.Errorf("NewImageMatrix returned both an image and an error for an image of incompatible height")
+	}
+}
+
+func TestgenSubImagesRectanglesReturnsCorrectNumberOfRectangles(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(100, 100, 300, 700))
+	im, err := NewImageMatrix(input)
+	if err != nil {
+		t.Fatalf("Error creating ImageMatrix: %s", err)
+	}
+	numRec := len(im.genSubImageRectangles())
+	if numRec != 4 {
+		t.Errorf("genSubImageRectangles() doesn't have correct number of rectangles: %d", numRec)
+	}
+}
+
+func TestgenSubImagesRectanglesReturnsCorrectCoordinates(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(0, 0, 400, 450))
+	coord := []image.Point{{0, 0},{200, 0}, {0, 150}, {200, 150}, {0, 300}, {200, 300}}
+	im, _ := NewImageMatrix(input)
+	listRec := im.genSubImageRectangles()
+	for i := range coord {
+		if coord[i] != listRec[i].Min {
+			t.Errorf("Rectangle coordinate (%d, %d )doesn't match with (%d, %d)", listRec[i].Min.X, listRec[i].Min.Y, coord[i].X, coord[i].Y)
+		}
+	}
+}
+
+func TestSubImagesReturnsExpectedImages(t *testing.T) {
+	input := image.NewNRGBA(image.Rect(0, 0, 400, 300))
+
+	blue := color.NRGBA{0, 0, 255, 255}
+	green := color.NRGBA{0, 255, 0, 255}
+	red := color.NRGBA{255, 0, 0, 255}
+	purple := color.NRGBA{200, 0, 200, 255}
+
+	draw.Draw(input, image.Rect(0, 0, 200, 150), &image.Uniform{blue}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(200, 0, 400, 150), &image.Uniform{green}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(0, 150, 200, 300), &image.Uniform{red}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(200, 150, 400, 300), &image.Uniform{purple}, image.ZP, draw.Src)
+
+	im, err := NewImageMatrix(input)
+        if err != nil {
+                t.Fatalf("Error creating ImageMatrix: %s", err)
+        }
+
+	imgRec := im.SubImages()
+
+	for i := 0; i < 4; i++ {
+		b := imgRec[i].Bounds()
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				if imgRec[i].At(x, y) != input.At(x, y) {
+					t.Errorf("Images are not the same at (%d, %d) point", x, y)
+				}
+			}
+		}
+
+	}
+}
