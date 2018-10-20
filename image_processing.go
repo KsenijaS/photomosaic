643b8dcb062afe6b98980c897f644cc049480481
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2018-10-19 21:40:36 -0700

    Updated image_processing and image_processing_test files

--- image_processing.go
+++ image_processing.go
@@ -1,11 +1,27 @@
 package photomosaic
 
 import (
+    "os"
+    "io/ioutil"
     "image"
     "image/color"
+    "image/jpeg"
+    "math"
+    "fmt"
+    "strings"
 )
 
-func ColorFractions(img image.Image) (rPix float32, gPix float32, bPix float32)  {
+
+type ImageSignature struct {
+    r, g, b float64
+}
+
+type IndexedImages struct {
+    images []image.Image
+    signatures []ImageSignature
+}
+
+func NewImageSignature(img image.Image) ImageSignature {
     var sumr, sumg, sumb uint64
     var c color.Color
 
@@ -21,7 +37,97 @@ func ColorFractions(img image.Image) (rPix float32, gPix float32, bPix float32) 
         }
     }
 
-    s := sumr+sumg+sumb
+    s := sumr+sumg+sumb+1
 
-    return float32(sumr)/float32(s), float32(sumg)/float32(s), float32(sumb)/float32(s)
+    return ImageSignature{float64(sumr)/float64(s), float64(sumg)/float64(s), float64(sumb)/float64(s)}
+}
+
+func (p *ImageSignature) distance(other *ImageSignature) float64 {
+    return math.Abs(p.r-other.r) + math.Abs(p.g-other.g) + math.Abs(p.b-other.b)
+}
+
+func (p *ImageSignature) debugString() string {
+    return fmt.Sprintf("%f:%f:%f", p.r, p.g, p.b)
+}
+
+func init() {
+    image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
+}
+
+func NewIndexedImages(dirpath string) (IndexedImages, error) {
+    var indexImages IndexedImages
+    var errstrings []string
+
+    files, err := ioutil.ReadDir(dirpath)
+    if err != nil {
+        return IndexedImages{nil, nil}, err
+    }
+
+    count := len(files)
+
+    indexImages.images = make([]image.Image, count)
+    indexImages.signatures = make([]ImageSignature, count)
+    errs := make([]error, count)
+
+    collector := make(chan struct{img image.Image; sgn ImageSignature; err error})
+    for i := range(files) {
+        go indexImage(dirpath+"/"+files[i].Name(), collector)
+    }
+    for i := 0; i < count; i++ {
+        tuple := <-collector
+        indexImages.images[i] = tuple.img
+        indexImages.signatures[i] = tuple.sgn
+        errs[i] = tuple.err
+    }
+
+    for i := range(errs) {
+        if errs[i] != nil {
+            errstrings = append(errstrings, errs[i].Error())
+        }
+    }
+
+    if errstrings != nil {
+        reterr := fmt.Errorf(strings.Join(errstrings, "\n"))
+        return IndexedImages{nil, nil}, reterr
+    }
+
+    return indexImages, nil
+}
+
+func indexImage(filename string, out chan<- struct{img image.Image; sgn ImageSignature; err error} ) {
+
+    file, err := os.Open(filename)
+    if err != nil {
+        out <- struct{img image.Image; sgn ImageSignature; err error}{nil, ImageSignature{0.0, 0.0, 0.0}, err}
+        return
+    }
+    defer file.Close()
+
+    img, _, err := image.Decode(file)
+    if err != nil {
+        out <- struct{img image.Image; sgn ImageSignature; err error}{nil, ImageSignature{0.0, 0.0, 0.0}, err}
+        return
+    }
+
+    signature := NewImageSignature(img)
+
+    out <- struct{img image.Image; sgn ImageSignature; err error}{img, signature, nil}
+}
+
+func (p *IndexedImages) FindClosest(img image.Image) image.Image {
+    var mini int
+    var d float64
+
+    minIndex := math.MaxFloat64
+    imageSignature := NewImageSignature(img)
+
+    for i := range(p.images) {
+        d = p.signatures[i].distance(&imageSignature)
+        if minIndex > d {
+            minIndex = d
+            mini = i
+        }
+    }
+
+    return p.images[mini]
 }
--- image_processing_test.go
+++ image_processing_test.go
@@ -1,13 +1,20 @@
 package photomosaic
 
 import (
+    "os"
+    "io/ioutil"
     "image"
     "image/color"
     "image/draw"
     "testing"
+    "math"
 )
 
-func TestColorFractionsReturnsCorrectNumbers(t *testing.T) {
+func almostEqual(x float64, y float64) bool {
+    return math.Abs(x-y) < 0.0001
+}
+
+func TestNewImageSignatureReturnsCorrectNumbers(t *testing.T) {
     input := image.NewNRGBA(image.Rect(0,0, 600, 150))
 
     blue := color.NRGBA{0, 0, 255, 255}
@@ -18,15 +25,14 @@ func TestColorFractionsReturnsCorrectNumbers(t *testing.T) {
     draw.Draw(input, image.Rect(200, 0, 400, 150), &image.Uniform{green}, image.ZP, draw.Src)
     draw.Draw(input, image.Rect(400, 0, 600, 150), &image.Uniform{red}, image.ZP, draw.Src)
 
-    r, g, b := ColorFractions(input)
+    sign := NewImageSignature(input)
 
-    if r != 1.0/3 || g != 1.0/3 || b != 1.0/3 {
-        t.Errorf("The concentration of red, blue or green color is not correct r %f, g %f, b %f", r,g,b)
+    if !almostEqual(sign.r, 1.0/3) || !almostEqual(sign.g, 1.0/3) || !almostEqual(sign.b, 1.0/3) {
+        t.Errorf("The concentration of red, blue or green color is not correct r %f, g %f, b %f", sign.r, sign.g, sign.b)
     }
 }
 
-
-func TestColorFractionsReturnsCorrectNumbersOnlyBlueAndRed(t *testing.T) {
+func TestNewImageSignatureReturnsCorrectNumbersOnlyBlueAndRed(t *testing.T) {
     input := image.NewNRGBA(image.Rect(0,0, 600, 150))
 
     blue := color.NRGBA{0, 0, 255, 255}
@@ -37,10 +43,115 @@ func TestColorFractionsReturnsCorrectNumbersOnlyBlueAndRed(t *testing.T) {
     draw.Draw(input, image.Rect(200, 0, 400, 150), &image.Uniform{black}, image.ZP, draw.Src)
     draw.Draw(input, image.Rect(400, 0, 600, 150), &image.Uniform{red}, image.ZP, draw.Src)
 
-    r, g, b := ColorFractions(input)
+    sign := NewImageSignature(input)
 
-    if r != 1.0/2 || g != 0.0 || b != 1.0/2 {
-        t.Errorf("The concentration of red, blue or green color is not correct r %f, g %f, b %f", r,g,b)
+    if !almostEqual(sign.r, 1.0/2) || !almostEqual(sign.g, 0.0) || !almostEqual(sign.b, 1.0/2) {
+        t.Errorf("The concentration of red, blue or green color is not correct r %f, g %f, b %f", sign.r, sign.g, sign.b)
     }
 }
 
+func TestImageColorDistanceReturnsCorrectDistance(t *testing.T) {
+    input := image.NewNRGBA(image.Rect(0,0, 600, 150))
+
+    blue := color.NRGBA{0, 0, 255, 255}
+    red := color.NRGBA{255, 0, 0, 255}
+    green := color.NRGBA{0, 255, 0, 255}
+
+    draw.Draw(input, image.Rect(0, 0, 200, 150), &image.Uniform{blue}, image.ZP, draw.Src)
+    draw.Draw(input, image.Rect(200, 0, 400, 150), &image.Uniform{green}, image.ZP, draw.Src)
+    draw.Draw(input, image.Rect(400, 0, 600, 150), &image.Uniform{red}, image.ZP, draw.Src)
+
+    sign := NewImageSignature(input)
+    d := sign.distance(&sign)
+
+    if !almostEqual(d, 0.0) {
+        t.Error("Color distance between images is not correct %f", d)
+    }
+}
+
+func TestNewIndexedImagesReturnCorrectValue(t *testing.T) {
+    index, err := NewIndexedImages("dir_test")
+    if err != nil {
+        t.Fatalf("Cannot create IndexedImages: %s", err)
+    }
+
+    if len(index.images) != 3 || len(index.signatures) != 3 {
+        t.Errorf("Length is not correct")
+    }
+
+    files, err := ioutil.ReadDir("dir_test")
+    if err != nil {
+        t.Fatalf("Cannot open directory")
+    }
+
+    isign := make([]ImageSignature, 3)
+    present := make([]int, 3)
+
+    for i := range(files) {
+        file, err := os.Open("dir_test/"+files[i].Name())
+        if err != nil {
+            t.Fatalf("Cannot open the file: %s", err)
+        }
+        defer file.Close()
+
+        idx, _, err := image.Decode(file)
+        if err != nil {
+            t.Fatalf("Cannot decode image: %s", err)
+        }
+
+        isign[i] = NewImageSignature(idx)
+    }
+
+    for i := 0; i < 3; i++ {
+        for j := 0; j < 3; j++ {
+            if index.signatures[j] == isign[i] {
+                if present[i] == 1 {
+                    t.Errorf("Duplicate image %s", isign[i].debugString())
+                }
+                present[i] = 1
+            }
+
+        }
+    }
+
+    for i := range(present) {
+        if present[i] == 0 {
+            t.Errorf("Image not found: %s", isign[i].debugString())
+        }
+    }
+
+}
+
+func TestFindClosestReturnsCorrectImage(t *testing.T) {
+    index, err := NewIndexedImages("dir_test")
+    if err != nil {
+        t.Fatalf("Cannot create IndexedImages: %s", err)
+    }
+
+    files, err := ioutil.ReadDir("dir_test")
+    if err != nil {
+        t.Fatalf("Cannot open directory")
+    }
+
+    file, err := os.Open("dir_test/"+files[0].Name())
+    if err != nil {
+        t.Fatalf("Cannot open the file: %s", err)
+    }
+    defer file.Close()
+
+    idx, _, err := image.Decode(file)
+    if err != nil {
+        t.Fatalf("Cannot decode image: %s", err)
+    }
+
+    img := index.FindClosest(idx)
+
+    sign1 := NewImageSignature(idx)
+    sign2 := NewImageSignature(img)
+
+    d := sign1.distance(&sign2)
+
+    if !almostEqual(d, 0.0) {
+        t.Errorf("Correct image is not found, image signature is %s instead of %s", sign2.debugString(), sign1.debugString())
+    }
+}
