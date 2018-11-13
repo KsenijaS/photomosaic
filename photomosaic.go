d38864c8b7ee9df0df17fdf79a331fb1ecb8a560
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2018-11-13 15:09:13 -0800

    Add photomosaic

---
+++ photomosaic.go
@@ -0,0 +1,23 @@
+package photomosaic
+
+import (
+	"github.com/disintegration/imaging"
+	"log"
+)
+
+func GenerateImageMatrixFromImages(img SubbableImage, i IndexedImages) (*ImageMatrix, error) {
+	imgmtx, err := NewImageMatrix(img)
+	if err != nil {
+		log.Fatalf("Cannot create Image Matrix %s", err)
+		return nil, err
+	}
+
+	outimgs := imgmtx.subImages()
+	for k := range outimgs {
+		outimgs[k] = imaging.Resize(i.FindClosest(outimgs[k]), 30, 30, imaging.Lanczos)
+	}
+
+	imgmtx.generateImageMatrix(outimgs)
+
+	return imgmtx, nil
+}
