c82e6c2f4dd55e3d9451f6523b395dfdb0c52110
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2018-11-13 15:50:09 -0800

    Update test so they reflect the new subimage size

--- image_matrix_test.go
+++ image_matrix_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestNewImageMatrixAcceptsImageOfSubimageSize(t *testing.T) {
-	input := image.NewNRGBA(image.Rect(100, 100, 300, 250))
+	input := image.NewNRGBA(image.Rect(100, 100, 400, 400))
 	im, err := NewImageMatrix(input)
 	if err != nil {
 		t.Errorf("Error while creating the image: %s", err)
@@ -37,7 +37,7 @@ func TestNewImageMatrixRejectsIncompatibleWidthImage(t *testing.T) {
 }
 
 func TestNewImageMatrixRejectsIncompatibleHeightImage(t *testing.T) {
-	input := image.NewNRGBA(image.Rect(100, 100, 300, 123))
+	input := image.NewNRGBA(image.Rect(100, 100, 400, 123))
 	im, err := NewImageMatrix(input)
 	if err == nil {
 		t.Errorf("NewImageMatrix accepted an image of incompatible height")
@@ -75,17 +75,17 @@ func TestgenSubImagesRectanglesReturnsCorrectCoordinates(t *testing.T) {
 }
 
 func TestSubImagesReturnsExpectedImages(t *testing.T) {
-	input := image.NewNRGBA(image.Rect(0, 0, 400, 300))
+	input := image.NewNRGBA(image.Rect(0, 0, 600, 600))
 
 	blue := color.NRGBA{0, 0, 255, 255}
 	green := color.NRGBA{0, 255, 0, 255}
 	red := color.NRGBA{255, 0, 0, 255}
 	purple := color.NRGBA{200, 0, 200, 255}
 
-	draw.Draw(input, image.Rect(0, 0, 200, 150), &image.Uniform{blue}, image.ZP, draw.Src)
-	draw.Draw(input, image.Rect(200, 0, 400, 150), &image.Uniform{green}, image.ZP, draw.Src)
-	draw.Draw(input, image.Rect(0, 150, 200, 300), &image.Uniform{red}, image.ZP, draw.Src)
-	draw.Draw(input, image.Rect(200, 150, 400, 300), &image.Uniform{purple}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(0, 0, 300, 300), &image.Uniform{blue}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(300, 0, 600, 300), &image.Uniform{green}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(0, 300, 300, 600), &image.Uniform{red}, image.ZP, draw.Src)
+	draw.Draw(input, image.Rect(300, 300, 600, 600), &image.Uniform{purple}, image.ZP, draw.Src)
 
 	im, err := NewImageMatrix(input)
 	if err != nil {
